cbae64bc015e5923e753b3c1b32a67409f4bae04
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2019-01-29 15:47:55 -0700

    Projects update

---
+++ js/github.js
@@ -0,0 +1,12 @@
+const url = "https://api.github.com/users/aluminumchassis/repos"
+const Http = new XMLHttpRequest();
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = function() {
+	e=JSON.parse(this.responseText)
+	console.log(e)
+	for (var i =0; i < e.length; i++ ){
+		console.log(e[i].name)
+		document.getElementById("hexGrid").innerHTML+='<li class="hex"><div class="hexIn"><a class="hexLink" href="'+e[i].html_url+'"><h1>'+e[i].name+'</h1><p>'+e[i].description+'</p></a></div></li>'
+	}
+}
