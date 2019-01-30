1af9534f0cb169d5deb9545149b95b8afa28d1a7
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2019-01-29 17:17:44 -0700

    styling, general visual improvements

--- js/github.js
+++ js/github.js
@@ -3,7 +3,7 @@ const Http = new XMLHttpRequest();
 Http.open("GET", url);
 Http.send();
 Http.onreadystatechange = function() {
-	e=JSON.parse(this.responseText)
+	e=JSON.parse(this.responseText.replace("AluminumChassis.github.io","Ciccarelli.me"))
 	console.log(e)
 	for (var i =0; i < e.length; i++ ){
 		console.log(e[i].name)
