51fd27399bb97ba5645a451f8b32360307d64068
Aluminumchassis <Aluminumchassis@gmail.com>
2019-03-19 20:20:27 -0700

    fixed title on chrome v2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if(window.location != "https://ciccarelli.me/") {
-	window.location = "https://ciccarelli.me/"
+	//window.location = "https://ciccarelli.me/"
 }
 const url = "https://api.github.com/users/aluminumchassis/repos"
 const Http = new XMLHttpRequest();
