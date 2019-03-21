f3667bb88189a388fed205d01bd13faf43856af2
Aluminumchassis <Aluminumchassis@gmail.com>
2019-03-21 13:21:11 -0700

    Service worker

--- QuickSW.js
+++ QuickSW.js
@@ -2,7 +2,7 @@
 if(typeof(document) != "undefined") {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('js/QuickSW.js').then(function(registration) {
+      navigator.serviceWorker.register('QuickSW.js').then(function(registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
         console.log('ServiceWorker registration failed: ', err);
