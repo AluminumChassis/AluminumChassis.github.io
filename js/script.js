cf05a95ff3487a3a1cd14de22e90a8a8285be23b
Aluminumchassis <Aluminumchassis@gmail.com>
2019-04-22 13:03:05 -0700

    fixed bugs and UI

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ function projects() {
 	overlay.style.display = "block"
 	overlay.style = "animation-name: slide; animation-play-state: running;";
 	setTimeout(function(){
-		document.getElementById("bg").style.backgroundImage="none"
-		document.getElementById("bg").style.backgroundColor= "#111"
+		document.getElementById("bg").style.display="none"
 
 		document.getElementById("projectButton").style.display = "none"
 		document.getElementById("skills").style.display = "block"
@@ -21,10 +20,13 @@ function projects() {
 		document.getElementById("title").innerText = "Projects"
 		for (var i =0; i < e.length; i++ ){
 			if(!document.getElementById(e[i].language)) {
-				document.getElementById("projects").innerHTML+='<div id="'+e[i].language+'" class="lang"><h1>'+e[i].language+'</h1></div>'
+				document.getElementById("projects").innerHTML+='<div id="'+e[i].language+'" class="lang"><h1>'+e[i].language+'<arrow>></arrow></h1></div>'
 			}
+		}
+		for (var i =0; i < e.length; i++ ){
 			document.getElementById(e[i].language).innerHTML+='<a target="_blank" href="'+e[i].html_url+'"><h1>' + e[i].name + '</h1>'+e[i].description+'</a>'
 		}
+			
 		var l = document.getElementsByClassName("lang")
 		for (var i = 0; i < l.length; i++) {
 			l[i].onmouseover = function(){
@@ -35,9 +37,11 @@ function projects() {
 				this.style = "height: 10vh";
 			};
 		}
-				
+		
 		setTimeout(function(){
 			overlay.style.display = "none"
+
+			document.getElementById("back").style.visibility = "visible"
 		},1500)
 			
 		
@@ -48,16 +52,16 @@ function mainPage() {
 	overlay.style.display = "block"
 	overlay.style = "animation-name: slideBack; animation-play-state: running;";
 	setTimeout(function(){
-		document.getElementById("bg").style.backgroundImage = ""
-		document.getElementById("bg").style.backgroundColor = ""
+		document.getElementById("bg").style.display="block"
 
-		document.getElementById("projectButton").style.display = "block"
 		document.getElementById("skills").style.display = "none"
 		document.getElementById("titleSlide").style.marginTop = "15%"
 		document.getElementById("title").innerHTML = "Hello, I'm Benjamin Ciccarelli<p> I'm a programmer from Arizona.</p>"
-		document.getElementById("projects").style.display = "none"
+		document.getElementById("projects").innerHTML = ""
 		setTimeout(function(){
 			overlay.style.display = "none"
+			document.getElementById("back").style.visibility = "hidden"
+			document.getElementById("projectButton").style.display = "block"
 		},1500)
 	},700)
 }
